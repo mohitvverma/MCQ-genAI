d6e2e10c9ea627e8c3bfd2c72f70881c08272f4c
mohitvverma <mohitvvermaa@Outlook.com>
2024-04-08 09:57:37 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'mcqgenerator',
+    version= '0.0.1',
+    author = 'Mohit verma',
+    author_email = 'mohitvverma@Outlook.com',
+    packages= find_packages()    # Creating the obj which find our packages.
+)
